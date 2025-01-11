23a32a07808c2802e7c38a6bca08ed57a5b8b82b
Aman Kumar <amankarguwal0@gmail.com>
2025-01-11 19:07:31 +0530

    Add button to ChaiPage for user interaction and adjust layout

--- shadcn/src/app/chai/page.tsx
+++ shadcn/src/app/chai/page.tsx
@@ -1,9 +1,10 @@
 function ChaiPage() {
   return (
-    <main
-    className="h-full flex justify-center items-center"
-    >
+    <main className="h-full flex justify-center items-center flex-col">
       <div>Chai Page 😃</div>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+        Test Button
+      </button>
     </main>
   );
 }
